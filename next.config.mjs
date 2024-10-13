13cdb36ccc9a3d5ddaf28c3af85218d945a2be05
jlietart <julien.lietart@gmail.com>
2024-10-13 11:25:42 +0100

    add github pages config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/website",
+  assetPrefix: "/website/",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
